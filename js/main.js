ba8ae80d9efca241c6f8ef0408b11363467170fe
Ivan <ivanrumanchev95@gmail.com>
2021-10-26 13:40:30 +0300

    Убрано смешение модулей, доработана логика отрисовки объявлений

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
-import './form.js';
+import {disableForm, enableForm, adFormList, mapFiltersList} from './form.js';
+import {createAds} from './data.js';
+import {createMap, CENTER_COORDINATES, mainMarker, createMarker, fillingMap} from './map.js';
 
-import './map.js';
+
+const address = document.querySelector('#address');
+
+disableForm(adFormList);
+disableForm(mapFiltersList);
+
+const map = createMap();
+
+let mapLoaded = false;
+map.on('load', () => {
+  address.value = `${CENTER_COORDINATES.lat}, ${CENTER_COORDINATES.lng}`;
+  mapLoaded = true;
+});
+
+fillingMap(map);
+
+if (mapLoaded) {
+  mainMarker.on('move', (evt) => {
+    address.value = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
+  });
+
+  window.onload = () => {
+    enableForm(adFormList);
+
+    const similarAds = createAds();
+    const commonMarkerGroup = L.layerGroup().addTo(map);
+    similarAds.forEach((similarAd) => {
+      createMarker(similarAd, commonMarkerGroup);
+    });
+
+    enableForm(mapFiltersList);
+  };
+}
--- js/map.js
+++ js/map.js
@@ -1,5 +1,3 @@
-import {disableForm, enableForm, adFormList, mapFiltersList} from './form.js';
-import {createAds} from './data.js';
 import {createCustomPopup} from './popup.js';
 
 const CENTER_COORDINATES = {
@@ -15,26 +13,6 @@ const COMMON_PIN_ICON_WIDTH = 40;
 const COMMON_PIN_ICON_HEIGHT = 40;
 const SCALE = 9;
 
-const address = document.querySelector('#address');
-
-disableForm(adFormList);
-disableForm(mapFiltersList);
-
-const map = L.map('map-canvas')
-  .on('load', () => {
-    enableForm(adFormList);
-    enableForm(mapFiltersList);
-    address.value = `${CENTER_COORDINATES.lat}, ${CENTER_COORDINATES.lng}`;
-  })
-  .setView(CENTER_COORDINATES, SCALE);
-
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(map);
-
 const createPinIcon = (pinIconWidth, pinIconHeight, url) => {
   const pinIcon = L.icon({
     iconUrl: url,
@@ -47,27 +25,8 @@ const createPinIcon = (pinIconWidth, pinIconHeight, url) => {
   return pinIcon;
 };
 
-const mainPinIcon = createPinIcon(MAIN_PIN_ICON_WIDTH, MAIN_PIN_ICON_HEIGHT, MAIN_PIN_ICON_URL);
-
-const mainMarker = L.marker(
-  CENTER_COORDINATES,
-  {
-    icon: mainPinIcon,
-    draggable: true,
-  },
-);
-
-mainMarker.addTo(map);
-
-mainMarker.on('move', (evt) => {
-  address.value = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
-});
-
-const commonMarkerGroup = L.layerGroup().addTo(map);
-const commonPinIcon = createPinIcon(COMMON_PIN_ICON_WIDTH, COMMON_PIN_ICON_HEIGHT, COMMON_PIN_ICON_URL);
-const similarAds = createAds();
-
-const createMarker = (similarAd) => {
+const createMarker = (similarAd, markerGroup) => {
+  const commonPinIcon = createPinIcon(COMMON_PIN_ICON_WIDTH, COMMON_PIN_ICON_HEIGHT, COMMON_PIN_ICON_URL);
   const lat = similarAd.location.lat;
   const lng = similarAd.location.lng;
   const commonMarker = L.marker({
@@ -79,10 +38,33 @@ const createMarker = (similarAd) => {
   });
 
   commonMarker
-    .addTo(commonMarkerGroup)
+    .addTo(markerGroup)
     .bindPopup(createCustomPopup(similarAd));
 };
 
-similarAds.forEach((similarAd) => {
-  createMarker(similarAd);
-});
+const mainPinIcon = createPinIcon(MAIN_PIN_ICON_WIDTH, MAIN_PIN_ICON_HEIGHT, MAIN_PIN_ICON_URL);
+
+const mainMarker = L.marker(
+  CENTER_COORDINATES,
+  {
+    icon: mainPinIcon,
+    draggable: true,
+  },
+);
+
+const createMap = () => L.map('map-canvas');
+
+const fillingMap = (map) => {
+  map.setView(CENTER_COORDINATES, SCALE);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+
+  mainMarker.addTo(map);
+};
+
+export {CENTER_COORDINATES, mainMarker, createMarker, createMap, fillingMap};
